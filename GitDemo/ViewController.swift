403dad45fe54074bba5994ff8c32b4a37b710b88
mangraa1 <georgiu.spare@gmail.com>
2023-04-18 09:32:50 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by mac on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
