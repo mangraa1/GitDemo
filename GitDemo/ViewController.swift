f00d0670ea2b0820375ebb37ea80f3a4a2ae6984
mangraa1 <georgiu.spare@gmail.com>
2023-04-18 13:35:20 +0300

    add func fix

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hfhfhfhfh")
+        
+        func fix(){
+            print("bud fixed")
+        }
     }
 
 
